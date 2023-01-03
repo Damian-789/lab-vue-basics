09d999bc3fd484adbbff7f0b53eca4ab98783de1
Damian-789 <isidorodamian@yahoo.com>
2023-01-04 00:45:28 +0100

    solved

---
+++ OneDrive/Documentos/Personales Lenovo/Labs Pendientes de enviar/Sem 9 - Clase 1 - Lab - VueJs - Basics/first-app/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Footer: Lorem ipsum dolor sit amet.</h2>
+  
+  <article>
+    <button>Log In</button>
+    <button>Log Out</button>
+  </article>
+
+
+</template>
+
+<script setup>
+
+
+
+
+</script>
+
+<style>
+
+</style>
