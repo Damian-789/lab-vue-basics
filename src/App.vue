e27cb056fa07a9624104a8ea6345554fbb174c23
Damian-789 <isidorodamian@yahoo.com>
2023-01-07 10:42:24 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <NavbarVue/>
+
+  <p v-bind:class="show"> Este texto es condicional</p>
+  <button @click="changeColor">Change Color</button>
+  <FooterVue/>
+</template>
+
+<script setup>
+import NavbarVue from './components/Navbar.vue';
+import FooterVue from './components/Footer.vue';
+import { ref } from 'vue';
+
+const show = ref("show");
+
+function changeColor(){
+  show.value="hidden";
+}
+
+
+</script>
+
+<style scoped>
+
+
+
+.show{
+background-color:white;
+
+}
+
+.hidden{
+  background-color: red;
+}
+.logo {
+
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <h3>Hola Mundo estoy haciendo un componente de Nav</h3>
+<p>Esta suma da {{(2+2)}}</p>
+<h3>Hola {{name}} ¿ves tu nombre en la pantalla? </h3>
+
+</template>
+
+
+
+
+<script setup>
+
+import {ref} from "vue"
+
+const name = ref("Pepe");
+
+/*const name = "Pepe";*/
+
+/*function changeName(){
+name.value = "Ramon";
+
+}
+*/
+
+
+
+
+</script>
+
+
+
+
+<style>
+
+</style>
